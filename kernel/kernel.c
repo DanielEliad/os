ee7d673d141a0301cfae64c89c94c9821dbf2988
Daniel Eliad <daniel.eliad32@gmail.com>
2016-08-24 02:46:39 +0300

    removed unneeded structs intended to avoid namespace_clashes. Will name variables in a unique way to avoid namespace clashes instead

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,5 @@
 #include "../drivers/screen.h"
 void main () {
-	Screen screen;
-	screen.clear_screen();
-	screen.print("I am the Tal OS...\nT.A.L.O.S\n    Tal\n    Awesome\n    Lovely\n    Overwatch\n    System");
+	clear_screen();
+	print("I am the Tal OS...\nT.A.L.O.S\n    Tal\n    Awesome\n    Lovely\n    Overwatch\n    System");
 }
