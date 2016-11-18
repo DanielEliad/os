6c7554682dd4aa89eab41dd4ce7b004b4a1db9fd
Daniel <daniel.eliad32@gmail.com>
2016-11-19 00:12:16 +0200

    started testing of memory management

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,6 +4,7 @@
 #include "irq.h"
 #include "timer.h"
 #include "keyboard.h"
+#include "../memory/header.h"
 
 void main () {
 	clear_screen();
@@ -14,6 +15,7 @@ void main () {
 	timer_install();
 	keyboard_install();
 	//timer_wait(5);
+	init_pages(0x40000000,0xe0000);
 	print("I am the Tal OS...\nT.A.L.O.S\n    Tal\n    Awesome\n    Lovely\n    Overwatch\n    System");
 	for(;;);
 }
