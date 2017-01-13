022b9bb62c0e9560cf9bc8eabbfea49ef746c350
Daniel Eliad <daniel.eliad32@gmail.com>
2017-01-13 20:16:15 +0200

    timerBuffer

--- kernel/timer.h
+++ kernel/timer.h
@@ -6,9 +6,11 @@
 #include "../drivers/screen.h"
 #include "utils.h"
 #define INPUT_TIMER_HZ 1193180 //In HZ
-
+#define timer_base (0x100000)
+struct TimerBuffer {
+  int timer_ticks;
+};
 void timer_phase(int hz);
-int timer_ticks;
 void timer_handler(struct regs *r);
 void timer_install();
 void timer_wait(int ticks);
